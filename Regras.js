fc960f37542a64c27c2d567114374157609f26f0
ALEX SANDRO DOS REIS CORDEIRO <alex.srcordeiro1@senacsp.edu.br>
2019-08-08 21:28:41 -0300

    Regras js

---
+++ Regras.js
@@ -0,0 +1,32 @@
+// Exemplo p/ não se fazer
+// let 1nome = ";
+
+// exemplo para se fazer 
+// string
+let nome = 'sandro'
+
+// tipo inteiro
+let idade = 27
+
+// tipo decimal
+let peso = 89
+
+// tipo boleano
+
+let visualizado = true;
+
+// Criar constante
+const nome1 = 'sandro'
+
+//exibir informacoes
+console.log('este é o console.log');
+
+console.log('Exibindo variáveis');
+console.log('Nome: ' + nome);
+nome = 'José';
+console.log('Nome: ' + nome);
+
+console.log('Nome const:' + nome)
+
+console.log('Nome: ' + nome + ' idade: '+ idade);
+console.log(`Nome: ${nome} idade ${idade}`)
